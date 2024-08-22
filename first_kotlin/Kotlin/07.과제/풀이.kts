92b4ab6237c2864f045146e3f4c292e09b00d8f7
seunghoon <rhfqoddl555@gmail.com>
2024-08-22 16:25:36 +0900

    12.과제

--- first_kotlin/Kotlin/07.과제/풀이.kts
+++ first_kotlin/Kotlin/07.과제/풀이.kts
@@ -1,17 +1,17 @@
 // 1번
-val A : Int = 10
-val B : Int = 10
-val C : Boolean = if(A==B) true else false
+val A: Int = 10
+val B: Int = 10
+val C: Boolean = if (A == B) true else false
 println(C)
 // 자료형이 다르면 비교연산자를 사용할 수 없다
 
 // 2번
-val AA:Int = 10
+val AA: Int = 10
 val BB: Int = AA * 2
 
 // 3번
-fun checkGrade(score: Int): String{
-    when(score){
+fun checkGrade(score: Int): String {
+    when (score) {
         in 90..100 -> return "A학점"
         in 80..89 -> return "B학점"
         in 70..79 -> return "C학점"
@@ -20,13 +20,13 @@ fun checkGrade(score: Int): String{
 }
 
 // 4번
-fun gradeTest(correctCount:Int): Int{
+fun gradeTest(correctCount: Int): Int {
     return correctCount * 5
 }
 
 // 5번
-fun plusTwoNumbers(firstNum: Int?, secondNum: Int?): Int{
-    val first: Int = if(firstNum == null) 0 else firstNum
-    val second: Int = if(secondNum == null) 0 else secondNum
+fun plusTwoNumbers(firstNum: Int?, secondNum: Int?): Int {
+    val first: Int = if (firstNum == null) 0 else firstNum
+    val second: Int = if (secondNum == null) 0 else secondNum
     return first + second
 }
