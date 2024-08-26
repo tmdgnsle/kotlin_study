b6bd256a43b37946f4016aade903cd22dd84b925
seunghoon <rhfqoddl555@gmail.com>
2024-08-26 11:55:35 +0900

    20.상속

--- first_kotlin/build.gradle.kts
+++ first_kotlin/build.gradle.kts
@@ -3,3 +3,6 @@ plugins {
     alias(libs.plugins.androidApplication) apply false
     alias(libs.plugins.jetbrainsKotlinAndroid) apply false
 }
+dependencies {
+    implementation(kotlin("script-runtime"))
+}
