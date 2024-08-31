6ffe960ca2bf1d799a4d3438d137362e0487e697
seunghoon <rhfqoddl555@gmail.com>
2024-08-31 23:48:47 +0900

    14.Drawable

--- kotlin_ui/app/src/main/java/com/example/kotlin_ui/MainActivity.kt
+++ kotlin_ui/app/src/main/java/com/example/kotlin_ui/MainActivity.kt
@@ -10,6 +10,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.linearlayout)
+        setContentView(R.layout.drawable)
     }
 }
