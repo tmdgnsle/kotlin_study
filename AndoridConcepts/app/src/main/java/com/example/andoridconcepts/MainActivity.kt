c198c407572f66902d54157f1341fbcc5a26f10b
seunghoon <rhfqoddl555@gmail.com>
2024-09-07 19:25:39 +0900

    01.Activity

---
+++ AndoridConcepts/app/src/main/java/com/example/andoridconcepts/MainActivity.kt
@@ -0,0 +1,53 @@
+package com.example.andoridconcepts
+
+import android.os.Bundle
+import android.util.Log
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+//        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+//        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+//            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+//            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+//            insets
+//        }
+        Log.d("LifeCycle", "onCreate")
+    }
+
+    override fun onStart() {
+        Log.d("LifeCycle", "onStart")
+        super.onStart()
+    }
+
+    override fun onResume() {
+        Log.d("LifeCycle", "onResume")
+        super.onResume()
+    }
+
+    override fun onPause() {
+        Log.d("LifeCycle", "onPause")
+        super.onPause()
+    }
+
+    override fun onStop() {
+        Log.d("LifeCycle", "onStop")
+        super.onStop()
+    }
+
+    override fun onDestroy() {
+        Log.d("LifeCycle", "onDestroy")
+        super.onDestroy()
+    }
+
+    override fun onRestart() {
+        Log.d("LifeCycle", "onRestart")
+        super.onRestart()
+    }
+
+}
