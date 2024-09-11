039b4d2d4d32a0c6f7e83464af88c7082144125b
seunghoon <rhfqoddl555@gmail.com>
2024-09-12 00:28:49 +0900

    07.Fragment

---
+++ AndoridConcepts/app/src/main/java/com/example/andoridconcepts/FragmentFirst.kt
@@ -0,0 +1,41 @@
+package com.example.andoridconcepts
+
+import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+
+class FragmentFirst : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // inflater : XML을 화면에 사용할 준비를 한다 (XML -> View로 만들어 준다)
+        // container : 프래그먼트에서 사용될 XML의 부모뷰
+        val view =  inflater.inflate(R.layout.first_fragment, container, false)
+        // attachToRoot : 루트뷰에 붙일지 말지 (X) 언제 붙을지(O)
+
+
+        (view.findViewById<TextView>(R.id.call_activity)).setOnClickListener {
+            (activity as FragmentActivity).printTestLog()
+        }
+
+
+        return view
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val data : String? = arguments?.getString("key")
+        Log.d("testt", "data is $data")
+    }
+
+    fun printTestLog(){
+        Log.d("testt", "print test log from fragment")
+    }
+}
