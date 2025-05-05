84a372313f1a49237cf1df682fdea275450f0b50
tmdgnsle <rhfqoddl555@gmail.com>
2025-05-05 18:31:34 +0900

    0505

--- Compose/part4-chapter3-11/app/src/main/java/kr/co/fastcampus/part4/chapter3_11/MainActivity.kt
+++ Compose/part4-chapter3-11/app/src/main/java/kr/co/fastcampus/part4/chapter3_11/MainActivity.kt
@@ -3,8 +3,11 @@ package kr.co.fastcampus.part4.chapter3_11
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
+import coil.compose.AsyncImage
+import coil.compose.rememberImagePainter
 import kr.co.fastcampus.part4.chapter3_11.ui.theme.CoilTheme
 
 class MainActivity : ComponentActivity() {
@@ -24,7 +27,21 @@ fun CoilEx() {
     // (Compose 한국어 문서의 추천, but Deprecated)
     // 이미지 URI: https://raw.githubusercontent.com/Fastcampus-Android-Lecture-Project-2023/part4-chapter3/main/part-chapter3-10/app/src/main/res/drawable-hdpi/wall.jpg
 
+//    val painter =
+//        rememberImagePainter(
+//            data = "https://raw.githubusercontent.com/Fastcampus-Android-Lecture-Project-2023/part4-chapter3/main/part-chapter3-10/app/src/main/res/drawable-hdpi/wall.jpg",
+//        ) // 지금은 404
+//    Image(painter = painter, contentDescription = "앤텔로프 캐년")
+
     // 스텝 4: AsyncImage를 이용해봅니다. model에 주소를 적으면 됩니다.
+    AsyncImage(
+        model = "https://raw.githubusercontent.com/Fastcampus-Android-Lecture-Project-2023/part4-chapter3/main/part-chapter3-10/app/src/main/res/drawable-hdpi/wall.jpg",
+        contentDescription = "앤텔로프 캐년",
+    )
+    AsyncImage(
+        model = "https://raw.githubusercontent.com/Fastcampus-Android-Lecture-Project-2023/part4-chapter3/main/part-chapter3-10/app/src/main/res/drawable-hdpi/wall.jpg",
+        contentDescription = "앤텔로프 캐년",
+    )
 }
 
 @Preview(showBackground = true)
--- Compose/part4-chapter3-12/app/src/main/java/kr/co/fastcampus/part4/chapter3_12/MainActivity.kt
+++ Compose/part4-chapter3-12/app/src/main/java/kr/co/fastcampus/part4/chapter3_12/MainActivity.kt
@@ -5,17 +5,26 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.Card
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.ColorPainter
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import coil.compose.AsyncImage
 import kr.co.fastcampus.part4.chapter3_12.ui.theme.CardTheme
 
 class MainActivity : ComponentActivity() {
@@ -62,9 +71,44 @@ fun CardEx(cardData: CardData) {
             // 스텝 1: `AsyncImage`, `Spacer`, `Column`, `Text`로
             // 레이아웃을 만들어보세요.
 
+//            AsyncImage(
+//                model = cardData.imageUri,
+//                contentDescription = cardData.imageDescription,
+//                modifier = Modifier.size(32.dp)
+//            )
+//            Spacer(modifier = Modifier.size(8.dp))
+//            Column {
+//                Text(
+//                    text = cardData.author
+//                )
+//                Spacer(modifier = Modifier.size(4.dp))
+//                Text(
+//                    text = cardData.description
+//                )
+//            }
+
             // 스텝 2: `AsyncImage`에는 `placeholder`를 지정하고,
             // `contentScale`을 `ContentScale.Crop`으로 설정합시다.
             // `clip(CircleShape)`로 둥근 외양을 만들어 봅시다.
+            AsyncImage(
+                model = cardData.imageUri,
+                placeholder = ColorPainter(placeHolderColor),
+                contentScale = ContentScale.Crop,
+                contentDescription = cardData.imageDescription,
+                modifier = Modifier
+                    .size(32.dp)
+                    .clip(CircleShape)
+            )
+            Spacer(modifier = Modifier.size(8.dp))
+            Column {
+                Text(
+                    text = cardData.author
+                )
+                Spacer(modifier = Modifier.size(4.dp))
+                Text(
+                    text = cardData.description
+                )
+            }
         }
     }
 }
